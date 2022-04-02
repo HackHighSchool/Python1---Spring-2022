de8643a7e88eb1439b3ea8532b99e5a380381a39
Dishita Thathapuram <dthathapuram@gmail.com>
2022-04-02 10:16:18 -0700

    Snake can Increase Length

--- snake_move.py
+++ snake_move.py
@@ -9,8 +9,10 @@ pygame.init()
 
 d_width = 900
 d_height = 700
+s_length = 30
 display = pygame.display.set_mode((d_width, d_height))
 pygame.display.update()
+
 
 end_of_game = False
 
@@ -29,39 +31,40 @@ y_change = 0
 
 food_color = (28, 127, 151)
 
-rectx = (random.randint(500, 500))
-recty = (random.randint(500, 500))
+foodx = (random.randint(100, 500))
+foody = (random.randint(100, 500))
 
-rect0 = Rect(rectx, recty, 10, 10)
+food = Rect(foodx, foody, 10, 10)
+
+snake_length = 20
+snake_width =  20
+
+snake= Rect (x, y, snake_length, snake_length)
 
 while not end_of_game:
     x += x_change
     y += y_change
-    display.fill(black)
-    pygame.draw.circle(display, color, (x, y), 11, 0)
-    time.sleep(0.1)
-    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and x_change != -5:
                 x_change = -5
                 y_change = 0
-            if event.key == pygame.K_LEFT and x_change == -5:
+            elif event.key == pygame.K_LEFT and x_change == -5:
                 print("left")
                 x_change = -5
                 y_change = 0
-            if event.key == pygame.K_RIGHT and x_change != -5:
+            elif event.key == pygame.K_RIGHT and x_change != -5:
                 print("right")
                 x_change = 5
                 y_change = 0    
-            if event.key == pygame.K_RIGHT and x_change == -5:
+            elif event.key == pygame.K_RIGHT and x_change == -5:
                 x_change = 5
                 y_change = 0
-            if event.key == pygame.K_UP and y_change != -5:
+            elif event.key == pygame.K_UP and y_change != -5:
                 print("up")
                 x_change = 0
                 y_change = -5
-            if event.key == pygame.K_UP and y_change == -5:
+            elif event.key == pygame.K_UP and y_change == -5:
                 x_change = 0
                 y_change = -5
             if event.key == pygame.K_DOWN and y_change != -5:
@@ -73,18 +76,40 @@ while not end_of_game:
                 y_change = 5
         
         if event.type == pygame.QUIT:
-            end_of_game = False
+            print ("Quit Event")
+            end_of_game = True
         
     if x >= d_width or x < 0 or y >= d_height or y < 0:
         end_of_game = True
         
-    pygame.draw.rect(display, food_color, rect0, 1)
-    pygame.display.update()
 
-    pygame.display.update()
+    display.fill(black)
+
+    food = pygame.draw.rect(display, food_color, food, 1)
+    
+    newsnake= Rect (x + x_change, y + y_change, snake_length, snake_width)
+
+    snake= pygame.draw.rect(display, color, newsnake) 
+
+
+#if pygame.Rect.collidedict(snake, food) == True:
+    #print ("Found Food!") 
 
 
 
+    time.sleep(0.1)
+    pygame.display.update() 
+
+
+    print ("X is ", x)
+    print ("Y is ", y) 
+
+    print ("Foodx is ", foodx)
+    print ("Foody is ", foody)
+
+    if (abs(x - foodx) <=10)  and (abs(y - foody) <=10):
+       print ("Found Food!") 
+       snake_length = snake_length + 1
 
 pygame.quit()
 quit()
